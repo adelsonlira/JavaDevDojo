c3cd0fa6410c705e459f73b538be32bbe34dcb0f
Adelson Lira <adelsonlira@yahoo.com.br>
2023-08-09 00:46:20 -0300

    "Orientação a objetos - Métodos pt 08"

--- src/br/com/adelson/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
+++ src/br/com/adelson/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -1,7 +1,15 @@
 package br.com.adelson.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Estudante {
+
     public String nome;
     public int idade;
     public char sexo;
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.sexo);
+        System.out.println("----------------");
+    }
 }
---
+++ src/br/com/adelson/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,24 @@
+package br.com.adelson.maratonajava.javacore.Bintroducaometodos.test;
+
+import br.com.adelson.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import br.com.adelson.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+
+        ImpressoraEstudante imprimir = new ImpressoraEstudante();
+
+        estudante01.nome = "Astrogildo";
+        estudante01.idade = 80;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Severina";
+        estudante02.idade = 78;
+        estudante02.sexo = 'F';
+
+        estudante02.imprime();
+        estudante01.imprime();
+    }
+}
