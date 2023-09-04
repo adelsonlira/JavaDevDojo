b032fa9b57a9741c02cdd6a942acc7bcb4edb951
Adelson Lira <adelsonlira@yahoo.com.br>
2023-09-04 12:56:43 -0300

    "Orientação Objetos - Enumeração pt 01 - Introdução"

---
+++ src/br/com/adelson/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -0,0 +1,6 @@
+package br.com.adelson.maratonajava.javacore.Kenum.dominio;
+
+public enum TipoCliente {
+    PESSOA_FISICA,
+    PESSOA_JURIDICA
+}
---
+++ src/br/com/adelson/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,18 @@
+package br.com.adelson.maratonajava.javacore.Kenum.test;
+
+import br.com.adelson.maratonajava.javacore.Kenum.dominio.Cliente;
+import br.com.adelson.maratonajava.javacore.Kenum.dominio.TipoCliente;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Fulaninho", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente2 = new Cliente("Ciclaninho", TipoCliente.PESSOA_FISICA);
+        Cliente cliente3 = new Cliente("Estrambolico", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente4 = new Cliente("Saracuri", TipoCliente.PESSOA_FISICA);
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+        System.out.println(cliente3);
+        System.out.println(cliente4);
+    }
+}
