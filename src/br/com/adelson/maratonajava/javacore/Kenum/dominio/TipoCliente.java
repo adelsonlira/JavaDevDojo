e1854a96f0cf52526fa8f836485acabab05e934a
Adelson Lira <adelsonlira@yahoo.com.br>
2023-09-05 23:52:41 -0300

    "Orientação Objetos - Enumeração pt 02 - Construtores e atributos"

--- src/br/com/adelson/maratonajava/javacore/Kenum/dominio/TipoCliente.java
+++ src/br/com/adelson/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,22 @@
 package br.com.adelson.maratonajava.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    private int Valor;
+    private String nomeRelatorio;
+
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.Valor = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public int getValor() {
+        return Valor;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
--- src/br/com/adelson/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/br/com/adelson/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -5,14 +5,12 @@ import br.com.adelson.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Fulaninho", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente2 = new Cliente("Ciclaninho", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Estrambolico", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente4 = new Cliente("Saracuri", TipoCliente.PESSOA_FISICA);
+        Cliente cliente1 = new Cliente("Fulaninho", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente2 = new Cliente("Ciclaninho", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+
     }
 }
